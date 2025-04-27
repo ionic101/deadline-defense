971d90628d1c113de1d7559157439695ffbd1b3d
ionic101 <iv.osipov2004@gmail.com>
2025-04-27 19:13:50 +0500

    Remove switch level system

--- Deadline Defense/Assets/Scripts/CompleteLevel.cs
+++ Deadline Defense/Assets/Scripts/CompleteLevel.cs
@@ -7,13 +7,11 @@ public class CompleteLevel : MonoBehaviour {
 	public string menuSceneName = "MainMenu";
 
 	public string nextLevel = "Level02";
-	public int levelToUnlock = 2;
 
 	public SceneFader sceneFader;
 
 	public void Continue ()
 	{
-		PlayerPrefs.SetInt("levelReached", levelToUnlock);
 		sceneFader.FadeTo(nextLevel);
 	}
 
