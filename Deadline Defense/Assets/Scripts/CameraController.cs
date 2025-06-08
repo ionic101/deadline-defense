1f007d8c331c13e0a8812332fbea1384d8424314
ionic101 <iv.osipov2004@gmail.com>
2025-06-09 01:46:25 +0500

    Fix camera move

--- Deadline Defense/Assets/Scripts/CameraController.cs
+++ Deadline Defense/Assets/Scripts/CameraController.cs
@@ -6,11 +6,11 @@ public class CameraController : MonoBehaviour {
 	public float panBorderThickness = 10f;
 
 	public float scrollSpeed = 5f;
-	public float minY = 10f;
-	public float maxY = 80f;
 
-	// Update is called once per frame
-	void Update () {
+	public Vector3 startAreaControll = new Vector3(-10, -10, -10);
+	public Vector3 endAreaControll = new Vector3(85, 10, -85);
+
+    void Update () {
 
 		if (GameManager.GameIsOver)
 		{
@@ -18,19 +18,19 @@ public class CameraController : MonoBehaviour {
 			return;
 		}
 
-		if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+		if ((Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) && transform.position.z < startAreaControll.z)
 		{
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
 		}
-		if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+		if ((Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) && transform.position.z > endAreaControll.z)
 		{
 			transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
 		}
-		if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+		if ((Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) && transform.position.x < endAreaControll.x)
 		{
 			transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
 		}
-		if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+		if ((Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) && transform.position.x > startAreaControll.x)
 		{
 			transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
 		}
@@ -40,7 +40,7 @@ public class CameraController : MonoBehaviour {
 		Vector3 pos = transform.position;
 
 		pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-		pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.y = Mathf.Clamp(pos.y, startAreaControll.y, endAreaControll.y);
 
 		transform.position = pos;
 
